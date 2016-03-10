92ce8bc65647b2ecd800d74cfea62f5fb6d3f8b3
Markus Fröschle <mfro@mubf.de>
2016-03-10 21:38:39 +0100

    add GPL headers and kudos to Kevin Cuzner

--- bdmdevice.h
+++ bdmdevice.h
@@ -1,3 +1,26 @@
+/*
+ * bdmdevice.h
+ *
+ * This file is part of tbdm.
+ *
+ * tbdm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tbdm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BaS_gcc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016        M. Froeschle
+ *
+ *
+ */
+
 #ifndef BDMDEVICE_H
 #define BDMDEVICE_H
 
--- teensybdmdevice.cpp
+++ teensybdmdevice.cpp
@@ -1,3 +1,26 @@
+/*
+ * teensybdmdevice.cpp
+ *
+ * This file is part of tbdm.
+ *
+ * tbdm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tbdm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BaS_gcc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016        M. Froeschle
+ *
+ *
+ */
+
 #include <QDebug>
 
 #include "teensybdmdevice.h"
@@ -121,7 +144,7 @@ void TeensyBDMDevice::close()
 
 int TeensyBDMDevice::sendCommand(BDMCommand &command)
 {
-    int ret;
+    int ret = 0;
 
     return ret;
 }
